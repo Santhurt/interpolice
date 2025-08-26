6f8953246b9e51fff1228ac2c23fcda043956ab3
santhurt <santiruizhurt@gmail.com>
2025-08-26 14:39:38 -0500

    add: actualizar planeta

--- backend/src/modules/planetas/planeta.controller.js
+++ backend/src/modules/planetas/planeta.controller.js
@@ -16,15 +16,22 @@ export async function traerPlanetas(req, res) {
     }
 }
 
-export async function traerPorId(req, res) {
+export async function traerPlanetaPorId(req, res) {
     try {
         const id = req.params.id;
-        const planeta = Planeta.findByPk(id);
+        const planeta = await Planeta.findByPk(id);
 
-        return res.status(200).json({
-            success: true,
-            data: planeta,
-        });
+        if (planeta) {
+            return res.status(200).json({
+                success: true,
+                data: planeta,
+            });
+        } else {
+            return res.status(400).json({
+                success: false,
+                message: "No se encontro el planeta",
+            });
+        }
     } catch (error) {
         console.log(error);
         return res.status(500).json({
@@ -42,6 +49,34 @@ export async function crearPlaneta(req, res) {
             success: true,
             data: nuevoPlaneta,
         });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+}
+
+export async function actualizarPlaneta(req, res) {
+    try {
+        const id = req.params.id;
+        const [rowCount] = await Planeta.update(req.body, {
+            where: {
+                id_planeta: id,
+            },
+        });
+
+        if (rowCount > 0) {
+            return res.status(200).json({
+                success: true,
+            });
+        } else {
+            return res.status(400).json({
+                success: false,
+                message: "No se actualizo ningun registro",
+            });
+        }
     } catch (error) {
         console.log(error);
         return res.status(500).json({
--- backend/src/modules/planetas/planeta.router.js
+++ backend/src/modules/planetas/planeta.router.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { crearPlaneta, traerPlanetas } from "./planeta.controller.js";
+import {
+    actualizarPlaneta,
+    crearPlaneta,
+    traerPlanetaPorId,
+    traerPlanetas,
+} from "./planeta.controller.js";
 
 const planetaRouter = express.Router();
 
 planetaRouter.get("/planetas", traerPlanetas);
+planetaRouter.get("/planetas/:id", traerPlanetaPorId);
 planetaRouter.post("/planetas", crearPlaneta);
+planetaRouter.put("/planetas/:id", actualizarPlaneta);
 
 export default planetaRouter;
