20e81d31a15cb6c2df0a8ff54fe19439795aa94c
santhurt <santiruizhurt@gmail.com>
2025-08-27 13:25:39 -0500

    Add: actualizar registro

--- backend/src/modules/historial/historial.controller.js
+++ backend/src/modules/historial/historial.controller.js
@@ -32,3 +32,50 @@ export async function traerHistoriales(req, res) {
         });
     }
 }
+export async function traerHistorialCiudadano(req, res) {
+    try {
+        const idCiudadano = req.params.id;
+        const hitoriales = await Historial.findAll({
+            where: {
+                id_ciudadano: idCiudadano,
+            },
+        });
+
+        return res.status(200).json({
+            success: true,
+            data: hitoriales,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+}
+
+export async function actualizarHistorialCiudadano(req, res) {
+    try {
+        const idCiudadano = req.params.id;
+        const filas = await Historial.update(req.body, {
+            where: { id_ciudadano: idCiudadano },
+        });
+
+        if (filas > 0) {
+            return res.status(200).json({
+                success: true,
+            });
+        } else {
+            return res.status(400).json({
+                success: false,
+                message: "No se actualizo ningun historial",
+            });
+        }
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: error.message,
+        });
+    }
+}
--- backend/src/modules/historial/historial.router.js
+++ backend/src/modules/historial/historial.router.js
@@ -1,9 +1,16 @@
 import express from "express";
-import { registrarHitorial, traerHistoriales } from "./historial.controller.js";
+import {
+    registrarHitorial,
+    traerHistoriales,
+    traerHistorialCiudadano,
+    actualizarHistorialCiudadano,
+} from "./historial.controller.js";
 
 const historialRouter = express.Router();
 
 historialRouter.get("/historiales", traerHistoriales);
+historialRouter.get("/historiales/:id", traerHistorialCiudadano);
 historialRouter.post("/historiales", registrarHitorial);
+historialRouter.put("/historiales/:id", actualizarHistorialCiudadano);
 
 export default historialRouter;
