d641271b274153a6e0b5ddfd11a267b4bc918463
santhurt <santiruizhurt@gmail.com>
2025-08-27 19:53:09 -0500

    Add: validaciones finales

--- backend/src/modules/historial/historial.model.js
+++ backend/src/modules/historial/historial.model.js
@@ -1,5 +1,6 @@
 import { DataTypes } from "sequelize";
 import sequelize from "../../config/database.js";
+import { santizarTexto } from "../helpers/sanitizacion.js";
 
 const Historial = sequelize.define(
     "hitoriales",
@@ -15,12 +16,23 @@ const Historial = sequelize.define(
         fecha: {
             type: DataTypes.DATE,
             defaultValue: DataTypes.NOW,
+            validate: {
+                isDate: {
+                    msg: "El formato de la fecha es incorrecto",
+                },
+            },
         },
         lugar: {
             type: DataTypes.STRING(45),
+            set(value) {
+                this.setDataValue(santizarTexto(value));
+            },
         },
         descripcion: {
             type: DataTypes.TEXT,
+            set(value) {
+                this.setDataValue(santizarTexto(value));
+            },
         },
     },
     {
